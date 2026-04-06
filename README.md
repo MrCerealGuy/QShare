8d8035cb970bb2562f2782965a23de15e9e299c4
Andreas Zahnleiter <a.zahnleiter@gmx.de>
2026-04-06 09:03:46 +0200

    06042026-0903

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 ```console
 pip install pyqrcode
 pip install pypng
+pip install flask
 ```
+
+
+
+flask run --host=0.0.0.0
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import http.server
+# -----------------------------------------------------------------------------
+# QShare
+# by Andreas Zahnleiter <a.zahnleiter@gmx.de>
+# -----------------------------------------------------------------------------
+# 2026-04-04 - az - created
+# -----------------------------------------------------------------------------
+
 import socket
 import socketserver
 import webbrowser
 import win32security
-import pyqrcode
 import os
+import pyqrcode
+from flask import Flask, request, render_template, url_for
+
 
 # -----------------------------------------------------------------------------
 
@@ -23,7 +31,7 @@ def authenticate(username, password):
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    ip = "http://" + s.getsockname()[0] + ":" + str(PORT) + "/login.html"
+    ip = "http://" + s.getsockname()[0] + ":" + str(PORT)
 
     return ip
 
@@ -31,41 +39,38 @@ def get_ip():
 
 def show_qr():
     fn_svg = os.path.join(os.environ['USERPROFILE'], 'myqr.svg')
-    fn_qrh = os.path.dirname(os.path.abspath(__file__)) + '/qr.html'
+    fn_qrh = os.path.dirname(os.path.abspath(__file__)) + '/templates/qr.html'
 
-    url = pyqrcode.create(IP)
+    url = pyqrcode.create(IP + "/login")
     url.svg(fn_svg, scale=8)
 
     webbrowser.open(fn_qrh)
 
 # -----------------------------------------------------------------------------
 
-def listen():
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        print("Type this in your Browser", IP)
-        print("or Use the QRCode")
-
-        httpd.serve_forever()
-
-        #authenticate(username, password):
-
-# -----------------------------------------------------------------------------
-
-# assigning the appropriate port value
-PORT = 8010
+app = Flask(__name__)
+PORT = 5100
 
 # changing the directory to access the files desktop with the help of os module
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# creating a http request
-Handler = http.server.SimpleHTTPRequestHandler
-
 # finding the IP address of the PC
 IP = get_ip()
 
+# show QR code
 show_qr()
 
-# continuous stream of data between client and server
-listen()
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['user']
+        return f"Hello {name}, POST request received"
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=PORT)
 
